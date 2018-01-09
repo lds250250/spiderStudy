844ee1858b33347cb2e9559f8ccafeb54c8fa2b9
多云 <30614302+lds250250@users.noreply.github.com>
2018-01-09 10:42:52 +0800

    猫眼爬虫2
    猫眼爬虫用类修改以后的版本

---
+++ 猫眼爬虫class.py
@@ -0,0 +1,55 @@
+import json
+import re
+
+import requests
+from requests.exceptions import RequestException
+
+
+class Spider():
+    def __init__(self):
+        self.Url = 'http://maoyan.com/board/4?offset='
+        self.root_pattern = (
+            '<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name">' +
+            '<a.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>' +
+            '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>')
+
+    def __get_one_page(self, offset):
+        Url = f'{self.Url}{offset}'
+        try:
+            response = requests.get(Url)
+            if response.status_code == 200:
+                return response.text
+            return None
+        except RequestException:
+            return None
+
+    def __parse_one_page(self, html):
+
+        pattern = re.compile(self.root_pattern, re.S)
+        items = re.findall(pattern, html)
+        for item in items:
+            yield {
+                'index': item[0],
+                'image': item[1],
+                'title': item[2],
+                'actor': item[3].strip()[3:],
+                'time': item[4].strip()[5:],
+                'score': item[5] + item[6]
+            }
+
+    def __write_to_file(self, content):
+        with open('maoyan100.txt', 'a', encoding="utf8") as f:
+            f.write(json.dumps(content, ensure_ascii=False) + '\n')
+            f.close()
+
+    def go(self, offset):
+        html = self.__get_one_page(offset)
+        for item in self.__parse_one_page(html):
+            print(item)
+            self.__write_to_file(item)
+
+
+if __name__ == '__main__':
+    spider = Spider()
+    for i in range(10):
+        spider.go(i * 10)
